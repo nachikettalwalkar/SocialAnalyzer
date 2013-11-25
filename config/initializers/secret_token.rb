754bb3a05e76c2ac7febb99e65ce005035c8f082
Nachiket <nachiket.talwalkar13@gmail.com>
2013-11-25 13:05:44 +0000

    Added Ruby project with Mongo setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialAnalyzer::Application.config.secret_token = '08315fe52e1f131a3806ff987d967dabb00360909ccf6cf9938554926aa134eed0305140c32e29e24f150859fac3b3f4906d907b7d4a295d7707bb2111f99d54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SocialAnalyzer::Application.config.session_store :cookie_store, key: '_socialAnalyzer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SocialAnalyzer::Application.config.session_store :active_record_store
